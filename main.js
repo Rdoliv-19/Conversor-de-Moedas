42bf74f7fd933b49437c55630a2afdc5b994903b
Rodrigo <digo.s.oliv@gmail.com>
2022-08-08 22:26:56 -0300

    Busca de elementos para montagem da função

---
+++ main.js
@@ -0,0 +1,13 @@
+function converter(){
+
+let valorDigitado = document.querySelector('#valor')
+let valor = valorDigitado.value;
+let valorDolarNumerico = parseFloat(valor);
+console.log(valorDolarNumerico)
+
+}
+
+
+
+
+
